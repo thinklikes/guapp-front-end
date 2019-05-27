fd166509f9ec1b208416a9e6e010c80f5b2ae471
guappFrontEnd <frontend@guapp.com>
2019-05-27 16:29:49 +0800

    delete tagsView

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -5,7 +5,6 @@
     <div :class="{hasTagsView:needTagsView}" class="main-container">
       <div :class="{'fixed-header':fixedHeader}">
         <navbar />
-        <tags-view v-if="needTagsView" />
       </div>
       <app-main />
       <right-panel v-if="showSettings">
@@ -17,7 +16,7 @@
 
 <script>
 import RightPanel from '@/components/RightPanel'
-import { AppMain, Navbar, Settings, Sidebar, TagsView } from './components'
+import { AppMain, Navbar, Settings, Sidebar } from './components'
 import ResizeMixin from './mixin/ResizeHandler'
 import { mapState } from 'vuex'
 
@@ -28,8 +27,7 @@ export default {
     Navbar,
     RightPanel,
     Settings,
-    Sidebar,
-    TagsView
+    Sidebar
   },
   mixins: [ResizeMixin],
   computed: {
