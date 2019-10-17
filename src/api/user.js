b4276341e09789e97e229bcbf553a67bb98cf9e2
Alfie.kuo <thinklikes@gmail.com>
2019-10-17 18:07:45 +0800

    do authentication

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,27 @@
 import request from '@/utils/request'
 
 export function login(data) {
-  return request({
-    url: '/user/login',
-    method: 'post',
-    data
-  })
+    return request({
+        url: process.env.VUE_APP_BACKEND_PREFIX + '/auth/login',
+        method: 'post',
+        data: {
+            'name': data.username,
+            'password': data.password
+        }
+    })
 }
 
 export function getInfo(token) {
-  return request({
-    url: '/user/info',
-    method: 'get',
-    params: { token }
-  })
+    return request({
+        url: process.env.VUE_APP_BACKEND_PREFIX + '/auth/me',
+        method: 'get',
+        params: { token }
+    })
 }
 
 export function logout() {
-  return request({
-    url: '/user/logout',
-    method: 'post'
-  })
+    return request({
+        url: process.env.VUE_APP_BACKEND_PREFIX + '/user/logout',
+        method: 'post'
+    })
 }
